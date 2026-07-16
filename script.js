34dbfb67395e5b3ab2135b2654c7e2acfbf24a6e
kaizererer <jeong@fit.edu.ph>
2026-07-17 00:09:04 +0800

    uploading js file

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('greetBtn').addEventListener('click', function() {
+    document.getElementById('message').innerText = "Hello! Thanks for visiting.";
+});
+
+// script.js file
+
+function guitarAlert() {
+    // This command tells the browser to change pages
+    window.location.href = 'about.html';
+}
